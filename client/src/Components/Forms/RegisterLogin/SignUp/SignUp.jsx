78b224801cb642db1ce9e48886df6df60ef2bd39
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-01-17 14:25:21 +0530

    update 1 / 17 1 2024

---
+++ client/src/Components/Forms/RegisterLogin/SignUp/SignUp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SignUp = () => {
+
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+
+
+
+
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default SignUp;
--- client/src/Pages/RegisterLogin/RegisterLogin.jsx
+++ client/src/Pages/RegisterLogin/RegisterLogin.jsx
@@ -1,16 +1,56 @@
 import CssRegisterLogin from './RegisterLogin.module.css'
 import Logo from '../../utils/Logo/Logo';
+import { useState } from 'react';
+import Login from '../../Components/Forms/RegisterLogin/Login/Login';
+import SignUp from '../../Components/Forms/RegisterLogin/SignUp/SignUp';
 
 const RegisterLogin = () => {
-  return (
-    <>
-    <div className={CssRegisterLogin.body} >
-        <div className={CssRegisterLogin.container}>
-            <Logo />
-        </div>
-    </div>
-    </>
-  )
+
+
+    const [activeForm, setActiveForm] = useState('signup');
+
+    const toggleForm = (formType) => {
+        setActiveForm(formType);
+    };
+
+
+
+
+    return (
+        <>
+            <div className={CssRegisterLogin.body} >
+                <div className={CssRegisterLogin.container}>
+                    <Logo />
+                    <br /><br /><br />
+                    <div className={CssRegisterLogin.toggleContainer}>
+                        <button
+                            className={`${CssRegisterLogin.toggleBtn} ${activeForm === 'signup' ? CssRegisterLogin.activeButton : ''}`}
+                            onClick={() => toggleForm('signup')}
+                        >
+                            Sign Up
+                        </button>
+
+                        <button
+                            className={`${CssRegisterLogin.toggleBtn} ${activeForm === 'login' ? CssRegisterLogin.activeButton : ''}`}
+                            onClick={() => toggleForm('login')}
+                        >
+                            Login
+                        </button>
+
+                    </div>
+                    <div className={`${CssRegisterLogin.signupForm} ${activeForm === 'signup' ? CssRegisterLogin.activeFormClass : CssRegisterLogin.inactiveFormClass}`}>
+                        <br />
+                        <SignUp />
+                    </div>
+
+                    <div className={`${CssRegisterLogin.loginForm} ${activeForm === 'login' ? CssRegisterLogin.activeFormClass : CssRegisterLogin.inactiveFormClass}`}>
+                        <br />
+                        <Login />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default RegisterLogin;
