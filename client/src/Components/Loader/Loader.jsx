ab3f8ba0f8a37888a6fefed9be3c89fc514797ab
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-01-21 19:35:12 +0530

    update working 21.1,2024 7:35

--- client/src/Components/Loader/Loader.jsx
+++ client/src/Components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
 
-    const src="assets/img/QuizzieLoader.svg";
+    const src="assets/img/QuizzieLoader311.svg";
 
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
