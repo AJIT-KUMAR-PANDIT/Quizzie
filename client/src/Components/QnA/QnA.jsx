ceef59107a21c7c30d68534969449b40925e039f
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-01-28 15:45:02 +0530

    revert2

--- client/src/Components/QnA/QnA.jsx
+++ client/src/Components/QnA/QnA.jsx
@@ -10,21 +10,13 @@ import { Url } from '../../utils/URL/Url';
 import axios from 'axios';
 
 const QnA = () => {
-
     const baseUrl = Url();
-
-    
-    // const [quizTitle, setQuizTitle] = useState("");
-    // const [quizeT, setQuizeT] = useState("Q & A");
-
-
     const { updateClose } = useContext(ContextModalClose);
-    const { quizData, updateUserId, quizTitle, updateTimer, updateQuestionTitle, updateOptionType } = useQuizContext();
+    const { quizData, updateUserId, updateTimer, updateQuestionTitle, updateOptionType } = useQuizContext();
     const [buttons, setButtons] = useState([1]);
     const [selectedOption, setSelectedOption] = useState('Text');
     const [clickedButtons, setClickedButtons] = useState(1);
 
-    // Handling the option text change and updateQuestionTitle
     const handleTimerClick = (buttonNumber) => {
         console.log(`Button ${buttonNumber} clicked!`);
         updateTimer(buttonNumber);
@@ -40,12 +32,12 @@ const QnA = () => {
             setButtons(prevButtons => [...prevButtons, prevButtons.length + 1]);
             setClickedButtons(prevButtons => prevButtons + 1);
         }
-       
     };
 
     const handleDeleteButtonClick = (buttonNumber) => {
         setButtons(prevButtons => prevButtons.filter(btn => btn !== buttonNumber));
-        setClickedButtons(prevButtons => prevButtons -1);
+        setClickedButtons(prevButtons => prevButtons - 1);
+        updateQuestionTitle([], buttonNumber); // Clear question data for the specific button
     };
 
     const handleCancelClick = () => {
@@ -53,155 +45,39 @@ const QnA = () => {
         updateClose(false);
     };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// +++++++++++++++++++++++++++++++ quiz create backend connect start
-
-const [quiz, setQuiz] = useState({
-    userId: "",
-    qNo: 0,
-    quizTitle: "",
-    quizType: "Q & A",
-    timer: "Off",
-    createDate: new Date().getTime(),
-    questionTitle: [
-      "What is the capital of France?"
-    ],
-    optionType: "Text",
-    options: [
-      "Paris London Berlin Madrid"
-    ],
-    imgUrl: [
-      " "
-    ],
-    correctAnswerIndex: 0,
-    attemptedCorrectly: 0,
-    attemptedIncorrectly: 0,
-    generatedUrl: " ",
-  });
-
-  
-
-  const beforeSubmit = () => {
-    updateUserId(localStorage.getItem("id"));
-    setQuiz([{
-      ...quiz,
-      userId: quizData.userId,
-      qNo: 0,
-      quizTitle: quizData.quizTitle,
-      quizType: quizData.quizeT,
-      timer: quizData.timer,
-      createDate: new Date().getTime(),
-      questionTitle: quizData.questionTitle,
-      optionType: quizData.optionType,
-      options: quizData.options,
-      imgUrl: quizData.imgUrl,
-    }])
-  }
-
-
-  const handleSubmit = async (e) => {
-    beforeSubmit();
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(`${baseUrl}/createQuizPoll`, quiz);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error.response.data);
-    }
-    console.log('Form submitted:', quiz);
-
-  };
-
-
-
-  // +++++++++++++++++++++++++++++++ quiz create backend connect end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        updateUserId(localStorage.getItem("id"));
+        try {
+            const response = await axios.post(`${baseUrl}/createQuizPoll`, { questions: quizData.questions });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error.response.data);
+        }
+        console.log('Form submitted:', quizData.questions);
+    };
 
     return (
         <div className={CssQnA.bodyQnA}>
-
-
-
-
-
-
-
-{
-        console.log("userid:", quizData.userId)
-      }
-      {
-        console.log("quizTitle:", quizData.quizTitle)
-      }
-      {
-        console.log("quizType:", quizData.quizeT)
-      }
-      {
-        console.log("timer:", quizData.timer)
-      }
-      {
-        console.log("createDate:", new Date().getTime())
-      }
-      {
-        console.log("questionTitle:", quizData.questionTitle)
-      }
-      {
-        console.log("optionType:", quizData.optionType)
-      }
-      {
-        console.log("options:", quizData.options)
-      }
-      {
-        console.log("imgUrl:", quizData.imgUrl)
-      }
-
-
-
-
-
-
-
-
-
             <div>
                 <div style={{ display: 'flex' }}>
-                    {buttons.map(buttonNumber => (
-                        <div key={buttonNumber} className={CssQnA.buttonContainer} >
-                            <button className={CssQnA.buttonAdded} onClick={() => setClickedButtons(buttonNumber)}>{buttonNumber}</button>
+                    {buttons.map((buttonNumber) => (
+                        <div key={buttonNumber} className={CssQnA.buttonContainer}>
+                            <button
+                                className={CssQnA.buttonAdded}
+                                onClick={() => setClickedButtons(buttonNumber)}
+                            >
+                                {buttonNumber}
+                            </button>
                             {buttonNumber !== 1 && (
-                                <span className={`${CssQnA.crossIcon} ${CssQnA.crossButton}`} onClick={() => handleDeleteButtonClick(buttonNumber)}>&#x2716;</span>
-                            )}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <span
+                                    className={`${CssQnA.crossIcon} ${CssQnA.crossButton}`}
+                                    onClick={() => handleDeleteButtonClick(buttonNumber)}
+                                >
+                                    &#x2716;
+                                </span>
+                            )}
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         </div>
                     ))}
                     <img src="assets/img/QnA+.svg" alt="QnA" onClick={handleImgClick} />
@@ -209,17 +85,20 @@ const [quiz, setQuiz] = useState({
                 </div>
                 <br />
                 <div>
-                    {buttons.map(buttonNumber => (
+                    {buttons.map((buttonNumber) => (
                         <input
                             key={buttonNumber}
                             id={`qTitle${buttonNumber}`}
                             type="text"
                             placeholder={`Question ${buttonNumber}`}
-                            className={`${CssQnA.inputQuestion} ${buttonNumber === clickedButtons ? CssQnA.show : CssQnA.hide}`}
-                            onChange={e => updateQuestionTitle(e.target.value, buttonNumber)}
+                            className={`${CssQnA.inputQuestion} ${
+                                buttonNumber === clickedButtons ? CssQnA.show : CssQnA.hide
+                            }`}
+                            onChange={(e) => updateQuestionTitle(e.target.value, buttonNumber)}
                         />
                     ))}
-                    <br /><br />
+                    <br />
+                    <br />
                     <div className={CssQnA.optionType}>
                         <label className={CssQnA.text2}>
                             <input
@@ -228,7 +107,7 @@ const [quiz, setQuiz] = useState({
                                 checked={selectedOption === 'Text'}
                                 onChange={handleOptionChange}
                                 className={CssQnA.radio1}
-                                id='radio1'
+                                id="radio1"
                             />
                             Text
                         </label>
@@ -239,7 +118,7 @@ const [quiz, setQuiz] = useState({
                                 checked={selectedOption === 'Image URL'}
                                 onChange={handleOptionChange}
                                 className={CssQnA.radio1}
-                                id='radio2'
+                                id="radio2"
                             />
                             Image URL
                         </label>
@@ -250,58 +129,67 @@ const [quiz, setQuiz] = useState({
                                 checked={selectedOption === 'Text & Image URL'}
                                 onChange={handleOptionChange}
                                 className={CssQnA.radio1}
-                                id='radio3'
+                                id="radio3"
                             />
                             Text & Image URL
                         </label>
-                    </div >
+                    </div>
                     <br />
                 </div>
 
                 <div style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
-                {buttons.map(buttonNumber => (
-                    <div key={buttonNumber} className={`${buttonNumber === clickedButtons ? CssQnA.show : CssQnA.hide}`}>
-                        {selectedOption === 'Text' && (
-                            <div
-                                id={`qNaOptions${buttonNumber}`}
-                                style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}
-                                className={` ${buttonNumber === buttons ? CssQnA.show : CssQnA.hide}`}
-                            >
-                                <div>
-                                    <QnAOptions />
+                    {buttons.map((buttonNumber) => (
+                        <div
+                            key={buttonNumber}
+                            className={`${buttonNumber === clickedButtons ? CssQnA.show : CssQnA.hide}`}
+                        >
+                            {selectedOption === 'Text' && (
+                                <div
+                                    id={`qNaOptions${buttonNumber}`}
+                                    style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}
+                                    className={` ${buttonNumber === buttons ? CssQnA.show : CssQnA.hide}`}
+                                >
+                                    <div>
+                                        <QnAOptions />
+                                    </div>
                                 </div>
+                            )}
 
-                            </div>
-                        )}
-
-                        {selectedOption === 'Image URL' && (
-                            <div id={`qNaImage${buttonNumber}`} style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
-                                <div>
-                                    <ImageOptions />
+                            {selectedOption === 'Image URL' && (
+                                <div
+                                    id={`qNaImage${buttonNumber}`}
+                                    style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}
+                                >
+                                    <div>
+                                        <ImageOptions />
+                                    </div>
                                 </div>
+                            )}
 
-                            </div>
-                        )}
-
-                        {selectedOption === 'Text & Image URL' && (
-                            <div id={`qNaTimg${buttonNumber}`} style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
-                                <div>
-                                    <TextImageOptions />
+                            {selectedOption === 'Text & Image URL' && (
+                                <div
+                                    id={`qNaTimg${buttonNumber}`}
+                                    style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}
+                                >
+                                    <div>
+                                        <TextImageOptions />
+                                    </div>
                                 </div>
-
-                            </div>
-                        )}
-
+                            )}
+                        </div>
+                    ))}
+                    <div>
+                        <Timer onTimerClick={handleTimerClick} />
                     </div>
-                ))}
-                <div>
-                    <Timer onTimerClick={handleTimerClick} />
-                </div>
                 </div>
                 <br />
                 <div style={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
-                    <button className={CssQnA.button5} onClick={handleCancelClick}>Cancel</button>
-                    <button className={CssQnA.button3} onClick={handleSubmit}> Create Quiz</button>
+                    <button className={CssQnA.button5} onClick={handleCancelClick}>
+                        Cancel
+                    </button>
+                    <button className={CssQnA.button3} onClick={handleSubmit}>
+                        Create Quiz
+                    </button>
                 </div>
             </div>
         </div>
